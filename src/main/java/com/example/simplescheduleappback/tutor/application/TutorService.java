77441af72f95714f6055889f44ec0c5757109455
jungsehui <jungsehui@naver.com>
2025-01-25 01:50:26 +0900

    feat: 선생님 정보 수정 메서드 구현

--- src/main/java/com/example/simplescheduleappback/tutor/application/TutorService.java
+++ src/main/java/com/example/simplescheduleappback/tutor/application/TutorService.java
@@ -3,8 +3,10 @@ package com.example.simplescheduleappback.tutor.application;
 import com.example.simplescheduleappback.tutor.domain.entity.Tutor;
 import com.example.simplescheduleappback.tutor.domain.repository.TutorRepository;
 import com.example.simplescheduleappback.tutor.dto.request.CreateTutorRequest;
+import com.example.simplescheduleappback.tutor.dto.request.UpdateTutorRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +14,7 @@ public class TutorService {
 
     private final TutorRepository tutorRepository;
 
+    @Transactional
     public Long registerTutor(CreateTutorRequest createTutorRequest) {
         Tutor tutor = Tutor.builder()
                 .name(createTutorRequest.name())
@@ -19,6 +22,21 @@ public class TutorService {
                 .username(createTutorRequest.username())
                 .password(createTutorRequest.password())
                 .build();
+
+        return tutorRepository.save(tutor).getTutorId();
+    }
+
+    @Transactional
+    public Long modifyTutor(Long tutorId, UpdateTutorRequest updateTutorRequest) {
+        Tutor tutor = tutorRepository.findByTutorId(tutorId)
+                .orElseThrow();
+
+        tutor.modifyTutor(
+                updateTutorRequest.name(),
+                updateTutorRequest.phoneNumber(),
+                updateTutorRequest.username(),
+                updateTutorRequest.password()
+        );
 
         return tutorRepository.save(tutor).getTutorId();
     }
