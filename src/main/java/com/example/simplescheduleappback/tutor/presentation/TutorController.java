3b352f42586dfd22718d15294d0ad3b9f6f1f9cc
jungsehui <jungsehui@naver.com>
2025-01-25 12:12:28 +0900

    feat: 선생님 CREATE, READ 부분 구현

--- src/main/java/com/example/simplescheduleappback/tutor/presentation/TutorController.java
+++ src/main/java/com/example/simplescheduleappback/tutor/presentation/TutorController.java
@@ -1,10 +1,29 @@
 package com.example.simplescheduleappback.tutor.presentation;
 
+import com.example.simplescheduleappback.tutor.application.TutorService;
+import com.example.simplescheduleappback.tutor.dto.request.CreateTutorRequest;
+import com.example.simplescheduleappback.tutor.dto.response.ReadTutorResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 public class TutorController {
 
+    private final TutorService tutorService;
+
+    @PostMapping("/tutors")
+    public ResponseEntity<Void> createTutor(@RequestBody CreateTutorRequest createTutorRequest) {
+        Long tutorId = tutorService.registerTutor(createTutorRequest);
+        return ResponseEntity.created(URI.create("/tutors/" + tutorId)).build();
+    }
+
+    @GetMapping("/tutors/{tutorId}")
+    public ResponseEntity<ReadTutorResponse> readTutor(@PathVariable Long tutorId) {
+        ReadTutorResponse readTutorResponse = tutorService.showTutor(tutorId);
+        return ResponseEntity.ok(readTutorResponse);
+    }
 }
